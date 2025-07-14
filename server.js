5de95b8f34f1ea2a7b97eac96a5befc320221109
David Ngacha <davidngacha01@gmail.com>
2025-07-14 21:22:58 +0300

    delivery to be called separately in frontend

--- server.js
+++ server.js
@@ -18,5 +18,8 @@ app.use('/api/admin', adminAuthRoutes);
 const orderRoutes = require('./routes/orders');
 app.use('/api/orders', orderRoutes);
 
+const deliveryRoutes = require('./routes/delivery');
+app.use('/api/delivery-fees', deliveryRoutes);
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
